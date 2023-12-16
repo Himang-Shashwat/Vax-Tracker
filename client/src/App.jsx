92334201b7f122515c6540b29ee8e997ed7e4ab5
Pranav Pore <pranav@pranavpore.com>
2023-12-16 12:57:05 +0530

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-slate-500 h-screen w-screen">
+      <h1 className="text-white text-3xl text-center p-2">VaxTracker</h1>
+    </div>
+  );
+}
+
+export default App;
