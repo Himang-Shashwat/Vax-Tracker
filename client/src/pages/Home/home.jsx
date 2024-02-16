7f823836bcb7207bdd114e518ce38b85ff7c58ad
Himang Shashwat <himangshashwat10@gmail.com>
2024-02-16 22:50:47 +0530

    Landing Page Changes

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -6,6 +6,26 @@ const Home = () => {
     <>
       <Navbar />
 
+      <div className='flex bg-blue-800 text-white mt-2 text-2xl font-bold rounded-lg'>
+        <h2 className='ml-2'>Vaccinate Your Children</h2>
+      </div>
+
+      <div className='flex'>
+        <div className='m-6 size-5/12 '>
+          <img className="rounded-lg" src="\src\assets\happy-family.png" alt="Happy Family" />
+        </div>
+        <div className=' card-body flex-1 p-6'>
+          <div className='shadow-2xl rounded-lg bg-gray-200'>
+            <h1 className='text-2xl font-bold items-center flex justify-center shadow-xl rounded-lg '>Why Vaccinate?</h1>
+            <h6 className='text-m m-2'>Vaccinating children is crucial for disease prevention,
+            community immunity, and reducing disease spread. It protects against serious illnesses,
+            prevents complications, and safeguards future generations. By vaccinating,
+            we ensure individual and community health, preventing outbreaks and maintaining overall well-being.</h6>
+          </div>
+        </div>
+      </div>
+
+      
       <div className="container mx-auto mt-8">
         <h1 className="text-3xl font-bold mb-4">Baby Vaccine Information</h1>
 
